111892128ac4b16c9fe5d5136430556a74429235
Marcelo <mtorres3@crimson.ua.edu>
2026-02-16 20:18:14 -0600

    updated journal to make main shet with future scores

--- lib/data_processing_utils.py
+++ lib/data_processing_utils.py
@@ -2,6 +2,7 @@ import nflreadpy as nfl
 import pathlib
 import polars as pl
 
+
 def get_player_fantasy_sheet(start_season=2016, end_season=2025) -> pl.DataFrame:
     # Shared variables
 
@@ -49,6 +50,7 @@ def get_player_fantasy_sheet(start_season=2016, end_season=2025) -> pl.DataFrame
 
     return get_relevant_player_data.collect()
 
+
 def get_team_fantasy_sheet(start_season=2016, end_season=2025) -> pl.DataFrame:
     seasons_to_retrieve = list(range(start_season, end_season + 1))
 
@@ -71,7 +73,6 @@ def get_team_fantasy_sheet(start_season=2016, end_season=2025) -> pl.DataFrame:
         'def_safeties',
     ]
 
-
     # Team Data
     # retrieval choices
     cols_to_retrieve = fantasy_dst_cols
@@ -88,10 +89,8 @@ def get_team_fantasy_sheet(start_season=2016, end_season=2025) -> pl.DataFrame:
     return get_relevant_team_data.collect()
 
 
-
 def get_combined_data(start_season=2016, end_season=2025) -> pl.DataFrame:
     # Shared variables
-
 
     player_data = get_player_fantasy_sheet(start_season, end_season)
     team_data = get_team_fantasy_sheet(start_season, end_season)
@@ -108,9 +107,66 @@ def get_combined_data(start_season=2016, end_season=2025) -> pl.DataFrame:
     combined_frame = (pl.concat(frames, how='diagonal_relaxed')
                       .fill_null(strategy='zero'))
 
+    combined_frame = (combined_frame.lazy().with_columns(
+        pl.when(pl.col('player_id').str.len_chars() > 0)
+        .then(pl.col('player_id'))
+        .otherwise(pl.col('team'))
+        .alias('id')
+    )).collect()
+
     # print(f"nulls: {combined_frame.null_count().sum_horizontal()}")
 
     # for column in combined_frame.columns:
     #     print(f"Column: {column} | Dtype: {combined_frame[column].dtype}")
 
     return combined_frame
+
+
+def add_fantasy_scoring_inplace(frame):
+    # create scoring column
+
+    class_scoring_expression = (
+        # passing
+            pl.col("passing_yards") / 25
+            + pl.col("passing_tds") * 4
+            + pl.col("passing_interceptions") * -2
+            + pl.col("passing_2pt_conversions") * 2
+            # rushing
+            + pl.col("rushing_yards") / 10
+            + pl.col("rushing_tds") * 6
+            + pl.col("rushing_fumbles_lost") * -2
+            + pl.col("rushing_2pt_conversions") * 2
+            # receiving
+            + pl.col("receptions") * 1
+            + pl.col("receiving_yards") / 10
+            + pl.col("receiving_tds") * 6
+            + pl.col("receiving_2pt_conversions") * 2
+            + pl.col("receiving_fumbles_lost") * -2
+            # kicking
+            + pl.col("pat_made")
+            + pl.col("fg_missed") * -1
+            + pl.col("fg_made_0_19") * 3
+            + pl.col("fg_made_20_29") * 3
+            + pl.col("fg_made_30_39") * 3
+            + pl.col("fg_made_40_49") * 4
+            + pl.col("fg_made_50_59") * 5
+            + pl.col("fg_made_60_") * 5
+            # defense / special teams
+            + pl.col("def_sacks")
+            + pl.col("def_interceptions") * 2
+            + pl.col("fumble_recovery_opp") * 2
+            + pl.col("def_tds") * 6
+            + pl.col("special_teams_tds") * 6
+            + pl.col("def_safeties") * 2
+        # TODO: use data aggregation to generate points allowed statistics NOTE: points allowed would require much data aggregation so it is left as a todo for now
+    ).alias("class_ppr_score")
+
+    frame.insert_column(len(frame.columns), class_scoring_expression)
+    return
+
+
+def output_frame(frame : pl.DataFrame, path : pathlib.Path):
+    # output as parquet and csv
+    print(frame.write_csv(str(path) + ".csv", include_header=True))
+    print(frame.write_excel(str(path) + ".xlsx", include_header=True))
+    print(frame.write_parquet(str(path) + ".parquet"))
